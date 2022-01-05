07c7b1c0ede8183ad50e6795446a4c6b8118be66
Andrew Rogers <andrewRogers1994@outlook.com>
2022-01-05 18:01:38 +0000

    Added Inital WEB Api Implementation

---
+++ ENSEK Web API/ENSEK Web API/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace ENSEK_Web_API.Models
+{
+    public class Account
+    {
+        public string AccountId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ ENSEK Web API/ENSEK Web API/Models/AccountContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ENSEK_Web_API.Models
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
